43b4f8af2f19705188476ff662f279024fd76fd6
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-19 13:25:47 +0200

    Lee Robinson: 03  using form in Next.js

---
+++ Lee_Robinson/03_using_form_in_nextjs/app/add-form.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+// import { useActionState } from "react";
+import { useFormState, useFormStatus } from "react-dom";
+import { createTodo } from "@/app/actions";
+
+const initialState = {
+  message: "",
+};
+
+//-----------------------
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button type="submit" aria-disabled={pending}>
+      Add
+    </button>
+  );
+}
+
+//-------------------------
+
+export function AddForm() {
+  // const [state, formAction] = useActionState(createTodo, initialState);
+  const [state, formAction] = useFormState(createTodo, initialState);
+
+  return (
+    <form action={formAction}>
+      <label htmlFor="todo">Enter Task</label>
+      <input type="text" id="todo" name="todo" required />
+      <SubmitButton />
+      <p aria-live="polite" className="sr-only" role="status">
+        {state?.message}
+      </p>
+    </form>
+  );
+}
---
+++ Lee_Robinson/03_using_form_in_nextjs/app/delete-form.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+// import { useActionState } from "react";
+import { useFormState, useFormStatus } from "react-dom";
+import { deleteTodo } from "@/app/actions";
+
+const initialState = {
+  message: "",
+};
+
+//-----------------------------------------------------
+
+function DeleteButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button type="submit" aria-disabled={pending}>
+      Delete
+    </button>
+  );
+}
+
+//-----------------------------------------------------
+
+export function DeleteForm({ id, todo }: { id: number; todo: string }) {
+  // const [state, formAction] = useActionState(deleteTodo, initialState);
+  const [state, formAction] = useFormState(deleteTodo, initialState);
+
+  return (
+    <form action={formAction}>
+      <input type="hidden" name="id" value={id} />
+      <input type="hidden" name="todo" value={todo} />
+      <DeleteButton />
+      <p aria-live="polite" className="sr-only" role="status">
+        {state?.message}
+      </p>
+    </form>
+  );
+}
